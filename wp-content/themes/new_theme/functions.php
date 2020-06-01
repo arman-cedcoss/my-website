6652ec47bc5a75371cfa03770226e0a3c7720cb3
arman-cedcoss <63393088+arman-cedcoss@users.noreply.github.com>
2020-06-01 21:54:24 +0530

    changes

--- wp-content/themes/new_theme/functions.php
+++ wp-content/themes/new_theme/functions.php
@@ -47,21 +47,13 @@ function register_menu() {
 
 add_action( 'init', 'register_menu' );
 
-/**
- * This is comment
- *
- * @package WordPress
- */
-if ( function_exists('add_theme_support' ) )
-{
-	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 150, 150, true );
-	// default Post Thumbnail dimensions (cropped)
-	// additional image sizes
-	// delete the next line if you do not need additional image sizes
-	add_image_size( 'category-thumb', 300, 9999 );
-	// 300 pixels wide (and unlimited height)
-}
+
+add_theme_support( 'post-thumbnails' );
+
+
+
+
+
 
 
 
