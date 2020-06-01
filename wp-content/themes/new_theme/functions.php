da8474660fe8736287cb3f962b16f467f991708f
arman-cedcoss <63393088+arman-cedcoss@users.noreply.github.com>
2020-06-01 22:01:27 +0530

    changes

--- wp-content/themes/new_theme/functions.php
+++ wp-content/themes/new_theme/functions.php
@@ -52,6 +52,24 @@ add_theme_support( 'post-thumbnails' );
 
 
 
+function themename_custom_header_setup() {
+    $args = array(
+        'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+        'default-text-color' => '000',
+        'width'              => 1000,
+        'height'             => 250,
+        'flex-width'         => true,
+        'flex-height'        => true,
+    )
+    add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+
+
+add_theme_support( 'custom-header' );
+
+
+
 
 
 
