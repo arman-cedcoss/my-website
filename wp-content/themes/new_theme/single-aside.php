80f35691fa058697c3afbf655178c514e2a87688
arman-cedcoss <63393088+arman-cedcoss@users.noreply.github.com>
2020-06-02 15:53:01 +0530

    latest CHANGE

--- wp-content/themes/new_theme/single-aside.php
+++ wp-content/themes/new_theme/single-aside.php
@@ -30,20 +30,20 @@ get_header();
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
-		?>
+        ?>
+        <?php	get_template_part('content',get_post_format()); ?>
 <!-- Blog Post -->
 <div class="card mb-4">
 	<img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
 	<div class="card-body">
 		<a href="<?php the_permalink(); ?>"><h2 class="card-title"><?php the_title(); ?></h2></a>
-		<div class="thumbnail-img"> <?php the_post_thumbnail('thumbnail'); ?></div>
 		<p class="card-text"><?php the_content(); ?></p>
 		<a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More &rarr;</a>
 	</div>
 	<div class="card-footer text-muted">
 		Posted on <a href="#"><?php the_date(); ?></a> by
 		<a href="#"><?php the_author(); ?></a>
-		<a href="#"><?php the_category(); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small></a>
+		<a href="#"><?php the_category(); ?></a>
 	</div>
 </div>
 
--- wp-content/themes/new_theme/single-image.php
+++ wp-content/themes/new_theme/single-image.php
@@ -26,24 +26,23 @@ get_header();
 <div class="col-md-8">
 
 <?php
-
 if ( have_posts() ) {
 	while ( have_posts() ) {
-		the_post();
-		?>
+        the_post(); ?>
+        <?php	get_template_part('content',get_post_format()); ?>
 <!-- Blog Post -->
 <div class="card mb-4">
 	<img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
 	<div class="card-body">
-		<a href="<?php the_permalink(); ?>"><h2 class="card-title"><?php the_title(); ?></h2></a>
-		<div class="thumbnail-img"> <?php the_post_thumbnail('thumbnail'); ?></div>
+<a href="<?php the_permalink(); ?>"><h2 class="card-title"><?php the_title(); ?></h2></a>
+<div class="thumbnail-img"> <?php the_post_thumbnail('thumbnail'); ?></div>
 		<p class="card-text"><?php the_content(); ?></p>
 		<a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More &rarr;</a>
 	</div>
 	<div class="card-footer text-muted">
 		Posted on <a href="#"><?php the_date(); ?></a> by
 		<a href="#"><?php the_author(); ?></a>
-		<a href="#"><?php the_category(); ?> || <?php the_tags(); ?> || <?php edit_post_link(); ?></small></a>
+		<a href="#"><?php the_category(); ?></a>
 	</div>
 </div>
 
@@ -51,9 +50,7 @@ if ( have_posts() ) {
 	}
 }
 ?>
-
-	</div>
-
+</div>
 <?php get_sidebar(); ?>
 
 </div>
